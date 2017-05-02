2d27fd6351057401cbca29606555128678865d80
Levi Lewis <levi@calmdev.com>
2017-05-02 16:22:22 -0400

    Display amenities on listing view

--- app/client/assets/js/components/Listings/Detail/index.js
+++ app/client/assets/js/components/Listings/Detail/index.js
@@ -1,9 +1,10 @@
 import capitalize from 'lodash/capitalize';
+import chunk from 'lodash/chunk';
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link, browserHistory } from 'react-router';
 import { bindActionCreators } from 'redux';
-import { Button, Divider, Grid, Icon, Item, Label, List, Segment } from 'semantic-ui-react';
+import { Button, Divider, Grid, Icon, Item, Label, List, Segment, Table } from 'semantic-ui-react';
 import { CircleMarker, Map, Popup, TileLayer } from 'react-leaflet';
 import * as actionCreators from 'actions';
 import { Loading, NProgress } from 'components';
@@ -29,13 +30,15 @@ export default class ListingsDetail extends React.Component {
   state = {
     isLoading: true,
     phone: false,
-    email: false
+    email: false,
+    showAmenities: false
   };
   constructor(props) {
 
     super(props);
     this.handleCallPhoneNumber = this.handleCallPhoneNumber.bind(this);
     this.handleWriteEmailAddress = this.handleWriteEmailAddress.bind(this);
+    this.handleShowAmenities = this.handleShowAmenities.bind(this);
   }
   componentDidMount() {
 
@@ -68,6 +71,10 @@ export default class ListingsDetail extends React.Component {
 
     this.setState({ email: this.props.listing.email });
   }
+  handleShowAmenities() {
+
+    this.setState({ showAmenities: !this.state.showAmenities });
+  }
   render() {
 
     const imageUrl = (this.state.isLoading === true || !this.props.listing.uploads[0]) ? '/assets/images/image.png' : `${this.props.listing.uploads[0].url}?width=480&height=398`;
@@ -89,6 +96,78 @@ export default class ListingsDetail extends React.Component {
         </CircleMarker>
       </Map>
     );
+    const amenityOptions = [
+      { key: 'alcohol', value: 'alcohol', text: 'Alcohol' },
+      { key: 'anchorage', value: 'anchorage', text: 'Anchorage' },
+      { key: 'bait_tackle', value: 'bait_tackle', text: 'Bait & Tackle' },
+      { key: 'beach', value: 'beach', text: 'Beach' },
+      { key: 'bike_rentals', value: 'bike_rentals', text: 'Bike Rentals' },
+      { key: 'boat_ramp', value: 'boat_ramp', text: 'Boat Ramp' },
+      { key: 'cable_tv_hookup', value: 'cable_tv_hookup', text: 'Cable TV Hookup' },
+      { key: 'car_rentals', value: 'car_rentals', text: 'Car Rentals' },
+      { key: 'club_house', value: 'club_house', text: 'Club House' },
+      { key: 'covered_slips', value: 'covered_slips', text: 'Covered Slips' },
+      { key: 'deep_water', value: 'deep_water', text: 'Deep Water' },
+      { key: 'diesel', value: 'diesel', text: 'Diesel' },
+      { key: 'dinghy_dock', value: 'dinghy_dock', text: 'Dinghy Dock' },
+      { key: 'dockage', value: 'dockage', text: 'Dockage' },
+      { key: 'dog_park', value: 'dog_park', text: 'Dog Park' },
+      { key: 'dry_storage', value: 'dry_storage', text: 'Dry Storage' },
+      { key: 'electricity', value: 'electricity', text: 'Electricity' },
+      { key: 'engine_service', value: 'engine_service', text: 'Engine Service' },
+      { key: 'fitness_center', value: 'fitness_center', text: 'Fitness Center' },
+      { key: 'fixed_docks', value: 'fixed_docks', text: 'Fixed Docks' },
+      { key: 'floating_docks', value: 'floating_docks', text: 'Floating Docks' },
+      { key: 'fuel_dock', value: 'fuel_dock', text: 'Fuel Dock' },
+      { key: 'gas', value: 'gas', text: 'Gas' },
+      { key: 'golf', value: 'golf', text: 'Golf' },
+      { key: 'groceries', value: 'groceries', text: 'Groceries' },
+      { key: 'hotels', value: 'hotels', text: 'Hotels' },
+      { key: 'ice', value: 'ice', text: 'Ice' },
+      { key: 'internet', value: 'internet', text: 'Internet' },
+      { key: 'laundry', value: 'laundry', text: 'Laundry' },
+      { key: 'live_aboard_allowed', value: 'live_aboard_allowed', text: 'Live Aboard Allowed' },
+      { key: 'maintenance_facility', value: 'maintenance_facility', text: 'Maintenance Facility' },
+      { key: 'medical_facility', value: 'medical_facility', text: 'Medical Facility' },
+      { key: 'moorings', value: 'moorings', text: 'Moorings' },
+      { key: 'ocean_access', value: 'ocean_access', text: 'Ocean Access' },
+      { key: 'oil_disposal', value: 'oil_disposal', text: 'Oil Disposal' },
+      { key: 'pharmacy', value: 'pharmacy', text: 'Pharmacy' },
+      { key: 'pool', value: 'pool', text: 'Pool' },
+      { key: 'propane', value: 'propane', text: 'Propane' },
+      { key: 'propeller_service', value: 'propeller_service', text: 'Propeller Service' },
+      { key: 'pumpout', value: 'pumpout', text: 'Pump-out' },
+      { key: 'rack_storage', value: 'rack_storage', text: 'Rack Storage' },
+      { key: 'repair_crane', value: 'repair_crane', text: 'Repair Crane' },
+      { key: 'restaurants', value: 'restaurants', text: 'Restaurants' },
+      { key: 'restrooms', value: 'restrooms', text: 'Restrooms' },
+      { key: 'ship_store', value: 'ship_store', text: 'Ship Store' },
+      { key: 'showers', value: 'showers', text: 'Showers' },
+      { key: 'telephone_hookup', value: 'telephone_hookup', text: 'Telephone Hookup' },
+      { key: 'tennis', value: 'tennis', text: 'Tennis' },
+      { key: 'trash', value: 'trash', text: 'Trash' },
+      { key: 'trailer_storage', value: 'trailer_storage', text: 'Trailer Storage' },
+      { key: 'travel_lift', value: 'travel_lift', text: 'Travel Lift' },
+      { key: 'water_hookup', value: 'water_hookup', text: 'Water Hookup' },
+      { key: 'water_taxi', value: 'water_taxi', text: 'Water Taxi' },
+      { key: 'winter_storage', value: 'winter_storage', text: 'Winter Storage' },
+      { key: 'yacht_brokerage', value: 'yacht_brokerage', text: 'Yacht Brokerage' }
+    ];
+    const visibleAmenityOptions = (!this.state.showAmenities) ? amenityOptions.splice(0, 4) : amenityOptions;
+    const amenityRows = chunk(visibleAmenityOptions, 2).map((amenities) => {
+
+      if (amenities.length < 2) { // Fill last empty cell if necessary.
+        amenities.push({ key: null, value: '', text: '' });
+      }
+      const amenityCells = amenities.map((amenity) => {
+
+        const disabledCell = <Table.Cell disabled>{amenity.text}</Table.Cell>;
+        const positiveCell = <Table.Cell positive><Icon name='checkmark' /> {amenity.text}</Table.Cell>;
+        const hasAmenity = (this.props.listing.amenities && this.props.listing.amenities.includes(amenity.value));
+        return hasAmenity ? positiveCell : disabledCell;
+      });
+      return <Table.Row>{amenityCells}</Table.Row>;
+    });
 
     return (this.state.isLoading === true) ? <Segment attached='bottom'><Loading /></Segment> :
       <div>
@@ -156,7 +235,22 @@ export default class ListingsDetail extends React.Component {
             />
           </Divider>
           <Segment>
-
+            <Table columns={2} className='center aligned' size='small' striped celled unstackable>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell colSpan={2}>
+                    Amenities & Services
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {amenityRows}
+              </Table.Body>
+            </Table>
+            <Button fluid
+              content={(this.state.showAmenities) ? 'Show Less' : 'Show All'}
+              onClick={this.handleShowAmenities}
+            />
           </Segment>
           <Divider horizontal>
             <Icon circular
